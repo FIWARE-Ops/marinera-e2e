87caf63443e2822c725749379fb25a4afca91d7f
Stefan Wiedemann <wistefan@googlemail.com>
2022-05-11 11:37:39 +0200

    verfiy

--- src/test/java/org/fiware/marinerae2e/StepDefinitions.java
+++ src/test/java/org/fiware/marinerae2e/StepDefinitions.java
@@ -3,6 +3,7 @@ package org.fiware.marinerae2e;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import okhttp3.HttpUrl;
 import okhttp3.MediaType;
@@ -26,26 +27,76 @@ import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 import java.util.Optional;
 
 import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class StepDefinitions {
 
-	private String remoteDriverUrl;
-	private String grafanaUrl;
-	private String brokerUrl;
-	private String quantumLeapUrl;
-	private String testTenant;
+	// name of the test entity to be created
+	private String testEntityId;
+	// the test relies on the datasource-checker dashboard. It needs to be available under the configured name.
+	private String datasourceCheckerDashboardName;
+	// the position of the tables inside the datagrid. The dashboard to test should have at least 2 tables:
+	// Historic data and current data
+	private String historicDataGridPosition;
+	private String currentDataGridPosition;
 
+	// url of the selenium instance to be used
+	private String remoteDriverUrl;
+	// url of the grafana to check
+	private String grafanaUrl;
+	// url of the context broker to be used for data creation
+	private String brokerUrl;
+	// url of quantum leap to send the notifications to
+	private String quantumLeapUrl;
+	// username of the grafana(admin)-user to use
 	private String grafanaUser;
+	// password of the grafana(admin)-user to use
 	private String grafanaPassword;
 
+	// holds the test subscription's location for later cleanup
 	private String subscriptionLocation;
 
-	static WebDriver webDriver;
+	// instance of the webdriver for contacting selenium
+	private static WebDriver webDriver;
+
+	/**
+	 * Reads the env and creates the connection to selenium.
+	 */
+	@Before
+	public void setUp() throws MalformedURLException {
+
+		readEnv();
+
+		ChromeOptions chromeOptions = new ChromeOptions();
+		webDriver = new RemoteWebDriver(new URL(remoteDriverUrl), chromeOptions);
+		webDriver.manage().timeouts().implicitlyWait(Duration.of(5, ChronoUnit.SECONDS));
+	}
+
+	/**
+	 * Read all env vars and set them. Else use the default values.
+	 */
+	private void readEnv() {
+		remoteDriverUrl = Optional.ofNullable(System.getenv("REMOTE_DRIVER_URL")).orElse("http://localhost:4444");
+		grafanaUrl = Optional.ofNullable(System.getenv("GRAFANA_URL")).orElse("http://localhost:3000");
+		brokerUrl = Optional.ofNullable(System.getenv("BROKER_URL")).orElse("http://localhost:1026");
+		quantumLeapUrl = Optional.ofNullable(System.getenv("QUANTUM_LEAP_URL")).orElse("http://quantumleap-quantumleap:8668");
+
+		testEntityId = Optional.ofNullable(System.getenv("TEST_ENTITY_ID")).orElse("test-air-quality");
+		datasourceCheckerDashboardName = Optional.ofNullable(System.getenv("DATASOURCE_CHECKER_DASHBOARD_NAME")).orElse("orion-datasource-checker");
+		datasourceCheckerDashboardName = Optional.ofNullable(System.getenv("DATASOURCE_CHECKER_DASHBOARD_NAME")).orElse("orion-datasource-checker");
+
+		historicDataGridPosition = Optional.ofNullable(System.getenv("HISTORIC_DATA_GRID_POSITION")).orElse("1");
+		currentDataGridPosition = Optional.ofNullable(System.getenv("CURRENT_DATA_GRID_POSITION")).orElse("2");
+
+		grafanaUser = Optional.ofNullable(System.getenv("GRAFANA_USERNAME")).orElse("fiwareAdmin");
+		grafanaPassword = Optional.ofNullable(System.getenv("GRAFANA_PASSWORD")).orElse("fiwareAdmin");
+	}
 
 	@When("A user opens Grafana.")
 	public void verify_forward_to_login_page() {
@@ -53,26 +104,11 @@ public class StepDefinitions {
 		assertEquals(String.format("%s/login", grafanaUrl), webDriver.getCurrentUrl(), "The user should be forwarded to the login-page.");
 	}
 
-	@Before
-	public void setUp() throws MalformedURLException {
-		remoteDriverUrl = Optional.ofNullable(System.getenv("REMOTE_DRIVER_URL")).orElse("http://localhost:4444");
-		grafanaUrl = Optional.ofNullable(System.getenv("GRAFANA_URL")).orElse("http://localhost:3000");
-		brokerUrl = Optional.ofNullable(System.getenv("BROKER_URL")).orElse("http://localhost:1026");
-		quantumLeapUrl = Optional.ofNullable(System.getenv("QUANTUM_LEAP_URL")).orElse("http://quantumleap-quantumleap:8668");
-		testTenant = Optional.ofNullable(System.getenv("TEST_TENANT")).orElse("test-tenant");
-
-		grafanaUser = Optional.ofNullable(System.getenv("GRAFANA_USERNAME")).orElse("fiwareAdmin");
-		grafanaPassword = Optional.ofNullable(System.getenv("GRAFANA_PASSWORD")).orElse("fiwareAdmin");
-		ChromeOptions chromeOptions = new ChromeOptions();
-		webDriver = new RemoteWebDriver(new URL(remoteDriverUrl), chromeOptions);
-		webDriver.manage().timeouts().implicitlyWait(Duration.of(5, ChronoUnit.SECONDS));
-	}
-
 	@Given("The subscription between Orion-LD and QuantumLeap is created.")
 	public void create_subscription() throws IOException {
 		OkHttpClient brokerClient = new OkHttpClient();
 
-		RequestBody subscriptionBody = RequestBody.create(getSubscriptionString(testTenant, quantumLeapUrl), MediaType.get("application/json"));
+		RequestBody subscriptionBody = RequestBody.create(getSubscriptionString(quantumLeapUrl), MediaType.get("application/json"));
 
 		Request subscriptionCreationRequest = new Request.Builder()
 				.url(String.format("%s/v2/subscriptions", brokerUrl))
@@ -86,7 +122,7 @@ public class StepDefinitions {
 		subscriptionLocation = response.header("Location");
 	}
 
-	@Given("Historical data is created.")
+	@Given("Some AirQuality data is created.")
 	public void push_historical_data_to_orion() throws IOException {
 		Instant now = Instant.now();
 		String patternFormat = "yyyy-MM-dd'T'hh:mm:ssZ";
@@ -111,7 +147,7 @@ public class StepDefinitions {
 					.addEncodedQueryParameter("options", "upsert")
 					.build();
 			Instant historicalNow = now.minus(Duration.of(5, ChronoUnit.MINUTES));
-			RequestBody entityBody = RequestBody.create(getTestEntity("test-air-quality", temp, humidity, co, no2, formatter.format(historicalNow)), MediaType.get("application/json"));
+			RequestBody entityBody = RequestBody.create(getTestEntity(testEntityId, temp, humidity, co, no2, formatter.format(historicalNow)), MediaType.get("application/json"));
 			Request entityCreationRequest = new Request.Builder()
 					.url(orionUrl)
 					.addHeader("Fiware-Service", "AirQuality")
@@ -141,21 +177,90 @@ public class StepDefinitions {
 
 	@When("The user navigates to the dashboard.")
 	public void move_to_dashboard() {
-		webDriver.get(String.format("%s/d/orion-datasource-checker/orion-datasource-checker?orgId=1", grafanaUrl));
+		webDriver.get(String.format("%s/d/%s/%s?orgId=1", grafanaUrl, datasourceCheckerDashboardName, datasourceCheckerDashboardName));
 		await("The user should now be at the orion-checker dashboard.").atMost(Duration.of(5, ChronoUnit.SECONDS)).until(() -> webDriver.getTitle().equals("Orion datasource checker - Grafana"));
 
 		WebElement panelHeader = webDriver.findElement(By.cssSelector("div.panel-header:nth-child(2) > header:nth-child(1) > div:nth-child(1) > h2:nth-child(1)"));
 		assertEquals("Timescale DB", panelHeader.getText(), "The timescale db panel should be visible.");
 	}
 
-	@After
-	public void cleanUp() {
-		webDriver.quit();
+	@Then("The current air-quality data should be visible.")
+	public void verfiy_current_data_is_visible() {
+		// we expect the current data panel to be the second in the grid
+		WebElement currentDataTable = webDriver.findElement(By.cssSelector(String.format("div.react-grid-item:nth-child(%s)", currentDataGridPosition)));
+		List<WebElement> elementListByEntityName = currentDataTable.findElements(By.xpath(String.format("//*[contains(text(), '%s')]", testEntityId)));
+		assertEquals(1, elementListByEntityName.size(), "The entity should be there exactly once.");
 	}
 
-	private String getSubscriptionString(String tenant, String quantumLeapAddress) {
+	@Then("The air-quality data history should be visible.")
+	public void verify_historic_data_is_visible() {
+		// we expect the historic panel to be the first in the grid
+		WebElement historicDataTable = webDriver.findElement(By.cssSelector(String.format("div.react-grid-item:nth-child(1)", historicDataGridPosition)));
+
+		await("Multiple entries for the test entity should exist in the historic table.")
+				.atMost(Duration.of(5, ChronoUnit.SECONDS))
+				.until(() -> historicDataTable.findElements(By.xpath(String.format("//*[contains(text(), '%s')]", testEntityId))).size() > 1);
+	}
+
+	/**
+	 * Removes all artifacts created and quits the
+	 */
+	@After
+	public void cleanUp() {
+		Optional<Response> subDeletionResponse = Optional.empty();
+		Optional<Response> qlDeletionResponse = Optional.empty();
+		Optional<Response> entityDeletionResponse = Optional.empty();
+
+		OkHttpClient httpClient = new OkHttpClient();
+		// remove subscription
+		Request subscriptionDeletion = new Request.Builder()
+				.url(String.format("%s/%s", brokerUrl, subscriptionLocation))
+				.method("DELETE", null)
+				.addHeader("Fiware-Service", "AirQuality")
+				.addHeader("Fiware-ServicePath", "/alcantarilla")
+				.build();
+		try {
+			subDeletionResponse = Optional.of(httpClient.newCall(subscriptionDeletion).execute());
+		} catch (Exception e) {
+		}
+
+		// remove all the data from ql
+		Request qlDeletion = new Request.Builder()
+				.url(String.format("%s/v2/entities/%s", quantumLeapUrl, testEntityId))
+				.method("DELETE", null)
+				.addHeader("Fiware-Service", "AirQuality")
+				.addHeader("Fiware-ServicePath", "/alcantarilla")
+				.build();
+		try {
+			qlDeletionResponse = Optional.of(httpClient.newCall(qlDeletion).execute());
+		} catch (Exception e) {
+		}
+		// remove data from orion-ld
+		Request entityDeletion = new Request.Builder()
+				.url(String.format("%s/v2/entities/%s", brokerUrl, testEntityId))
+				.method("DELETE", null)
+				.addHeader("Fiware-Service", "AirQuality")
+				.addHeader("Fiware-ServicePath", "/alcantarilla")
+				.build();
+		try {
+			entityDeletionResponse = Optional.of(httpClient.newCall(entityDeletion).execute());
+		} catch (Exception e) {
+		}
+		webDriver.quit();
+
+		if (!entityDeletionResponse.map(r -> !r.isSuccessful()).orElse(false) ||
+				!qlDeletionResponse.map(r -> !r.isSuccessful()).orElse(false) ||
+				!subDeletionResponse.map(r -> !r.isSuccessful()).orElse(false)) {
+			fail(String.format("Cleanup was not successfull: Entity: %s - QL: %s - Subscription: %s",
+					entityDeletionResponse.map(Response::code).map(String::valueOf).orElse("No response"),
+					qlDeletionResponse.map(Response::code).map(String::valueOf).orElse("No response"),
+					subDeletionResponse.map(Response::code).map(String::valueOf).orElse("No response")));
+		}
+	}
+
+	private String getSubscriptionString(String quantumLeapAddress) {
 		return String.format("{\n" +
-				"    \"description\": \"%s AQ - Orion to QuantumLeap subscription\",\n" +
+				"    \"description\": \"AQ - Orion to QuantumLeap subscription\",\n" +
 				"    \"subject\": {\n" +
 				"        \"entities\": [\n" +
 				"            {\n" +
@@ -188,7 +293,7 @@ public class StepDefinitions {
 				"            \"TimeInstant\"\n" +
 				"        ]\n" +
 				"    }\n" +
-				"}", tenant, quantumLeapAddress);
+				"}", quantumLeapAddress);
 	}
 
 	private String getTestEntity(String id, double temperature, double humidity, double co, double no2, String timeInstant) {
--- src/test/resources/testDefinition/verfiy_data_in_grafana.feature
+++ src/test/resources/testDefinition/verfiy_data_in_grafana.feature
@@ -1,8 +1,10 @@
 Feature: Verify data in grafana, through the broker.
 
-  Scenario: Login and verify
+  Scenario: Create AirQuality data, login to grafana and watch it.
     Given The subscription between Orion-LD and QuantumLeap is created.
-    And Historical data is created.
+    And Some AirQuality data is created.
     When A user opens Grafana.
     And The user logs into Grafana as an admin.
     And The user navigates to the dashboard.
+    Then The current air-quality data should be visible.
+    And The air-quality data history should be visible.
